8aac3eef3a2450e9f7b4d792bb20b93c5d39c397
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-09-03 17:09:02 +0530

    feat: first setup commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-yellow-200">
+      <Text>Open up to start working on your app!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
